255d1a2396d4007556af5d033faf7eca1e7fa566
AleksandrVed <alexandervhrom@gmail.com>
2021-09-16 18:09:03 +0300

    bb

--- bboard/bboard1/urls.py
+++ bboard/bboard1/urls.py
@@ -4,9 +4,10 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    path('', views.index.as_view(), name='main'),
+    path('', views.index, name='main'),
     path('<int:rubric_id>/all', views.by_rubric, name='by_rubric'),
     path('add/', views.BbCreateView.as_view(), name='add'),
+    url('announcement/(?P<pk>\d+)/edit', views.BbUpdateView.as_view(), name='edit'),
+    url('announcement/(?P<pk>\d+)/delete', views.BbDeleteView.as_view(), name='delete'),
     url('announcement/(?P<pk>\d+)', views.objec.as_view(), name='obj'),
-    #path('<int:rubric_id>/<int:pke>', views.objec.as_view(), name='obj'),
 ]
--- bboard/bboard1/views.py
+++ bboard/bboard1/views.py
@@ -4,32 +4,54 @@ from django.http import HttpResponse
 from .models import Bb, Rubric
 from . import forms
 from django.views.generic.edit import CreateView
-from django.views.generic import ListView, DetailView
+from django.views.generic import ListView, DetailView, UpdateView, DeleteView
 from django.urls import reverse_lazy
-#def index(request):
+from django.core.paginator import Paginator
+def index(request):
 ##    s = 'Список объявлений\r\n\r\n\r\n'
 ##    for bb in Bb.objects.order_by('-published'):
 ##        s += bb.title + '\r\n' + bb.content + '\r\n\r\n'
-#    bbs = Bb.objects.all()
-#    rub = Rubric.objects.all()
-#    return render(request, "main.html", context={'bbs': bbs, 'rubrics': rub,})
+    bbs = Bb.objects.all()
+    rub = Rubric.objects.all()
+    paginator = Paginator(bbs, 2)
+    if 'page' in request.GET:
+        page_num = request.GET['page']
+    else:
+        page_num = 1
+    page = paginator.get_page(page_num)
+    return render(request, "bboard1/bb_list.html", context={'obyavleniya': page.object_list, 'rubrics': rub,'page':page})
 
-class index(ListView):
+"""class index(ListView):
     model = Bb
     context_object_name = 'obyavleniya'
     rub = Rubric.objects.all()
-    extra_context ={'rubrics': rub}
 
+    paginator = Paginator(model, 2)
+    if 'page' in request.GET:
+        page_num = request.GET['page']
+
+    extra_context ={'rubrics': rub}
+"""
 def by_rubric(request, rubric_id):
     bbs = Bb.objects.filter(rubric=rubric_id)
     rubrics = Rubric.objects.all()
     current_rubric = Rubric.objects.get(pk=rubric_id)
-    context = {'bbs': bbs, 'rubrics': rubrics, 'current_rubric': current_rubric}
+
+    paginator = Paginator(bbs, 2)
+    if 'page' in request.GET:
+        page_num = request.GET['page']
+    else:
+        page_num = 1
+    page = paginator.get_page(page_num)
+
+    context = {'page':page,'bbs': page.object_list, 'rubrics': rubrics, 'current_rubric': current_rubric}
     return render(request, "by_rubric.html", context)
 
 class objec(DetailView):
     model = Bb
     template_name = 'detail.html'
+    rubrics = Rubric.objects.all()
+    extra_context = {'rubrics': rubrics}
 
 
 class BbCreateView(CreateView):
@@ -41,3 +63,13 @@ class BbCreateView(CreateView):
         context = super().get_context_data(**kwargs)
         context['rubrics']=Rubric.objects.all()
         return context
+
+class BbUpdateView(UpdateView):
+    model = Bb
+    template_name = 'update.html'
+    fields = '__all__'
+    success_url = reverse_lazy('main')
+
+class BbDeleteView(DeleteView):
+    model = Bb
+    success_url = reverse_lazy('main')
